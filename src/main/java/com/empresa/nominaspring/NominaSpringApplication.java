fa8d17a34a5da5abd4cd61449a085a01520f3776
frodfer716 <frodfer716@g.educaand.es>
2023-11-22 13:44:54 +0100

    Subida Nominas con Spring

---
+++ src/main/java/com/empresa/nominaspring/NominaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.nominaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NominaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NominaSpringApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/empresa/nominaspring/controller/EmpleadoController.java
@@ -0,0 +1,14 @@
+package com.empresa.nominaspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EmpleadoController {
+
+    @GetMapping("/empresa")
+    public String index() {
+        return "index";
+    }
+    
+}
