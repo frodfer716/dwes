a2a027765e71029fa11c8c2e67ea8cd1c7f5ae2c
frodfer716 <frodfer716@g.educaand.es>
2023-11-29 00:43:44 +0100

    cotinuacion proyecto.

--- src/main/java/com/empresa/nominaspring/controller/EmpleadoController.java
+++ src/main/java/com/empresa/nominaspring/controller/EmpleadoController.java
@@ -8,7 +8,6 @@ public class EmpleadoController {
 
     @GetMapping("/empresa")
     public String index() {
-        System.out.println("hola");
         return "index";
     }
     
--- src/main/java/com/empresa/nominaspring/service/EmpleadoServiceImpl.java
+++ src/main/java/com/empresa/nominaspring/service/EmpleadoServiceImpl.java
@@ -1,16 +1,21 @@
 package com.empresa.nominaspring.service;
 
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.empresa.nominaspring.entity.Empleado;
+import com.empresa.nominaspring.repository.EmpleadoRepository;
 
 @Service
 public class EmpleadoServiceImpl implements EmpleadoService {
 
+    @Autowired
+    private EmpleadoRepository empleadoRepository;
+
     @Override
     public List<Empleado> listarEmpleados() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'listarEmpleados'");
+        return empleadoRepository.findAll();
     }
 
 
